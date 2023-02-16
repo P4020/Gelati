6ddb15aea0d3f2d1a49f54c137fb61450a43a8e4
P4020 <paolofrancesco.pompa@studenti.ittsrimini.edu.it>
2023-02-16 10:35:08 +0100

    Prima Versione

---
+++ pompa.paolofrancesco.4h.gelati/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using pompa.paolofrancesco._4h.gelati.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pompa.paolofrancesco._4h.gelati
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string nomefile = "db.csv"; 
+                Gelati gg = new Gelati(nomefile);
+                dgDati.ItemsSource = gg;
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            ;
+        }
+    }
+}
